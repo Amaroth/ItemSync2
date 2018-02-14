615bc31e72fac51c1b85b539d3e5402eaa3757f0
Amaroth <Amaroth7@seznam.cz>
2018-02-14 22:36:48 +0100

    Initial GUI commit.

---
+++ ItemSync2/GUI/MainWindow.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ItemSync2.Core;
+
+namespace ItemSync2.GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        internal void LoadSettings()
+        {
+
+        }
+
+        private void dbcBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void hostBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void loginBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void passwordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void databaseBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void tableBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void portBox_ValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+
+        }
+
+        private void startIDBox_ValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+
+        }
+
+        private void endIDBox_ValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+
+        }
+
+        private void savePassBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_Drop(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void dbcButt_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void testConnButt_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void saveSettButt_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void helpButt_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void checkChangesButt_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void dbToDBCButt_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void dbcToDBButt_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
